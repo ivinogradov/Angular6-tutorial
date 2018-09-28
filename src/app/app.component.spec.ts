25018fda80bfe6dd874bcb5b11384b6a9199d819
ivinogradov <ilya.vngrdv@gmail.com>
2018-09-28 15:31:30 -0500

    Fix unit tests
    Summary:
    
    Reviewers: tyrell, patrickcharlesmayer, gilicaspi, zerodiff, alex, thefisch63
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeroesComponent
+      ],
+      imports: [
+        FormsModule
       ],
     }).compileComponents();
   }));
