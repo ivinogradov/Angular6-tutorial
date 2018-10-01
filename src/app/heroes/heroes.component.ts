a4054139114b54a02d351929436d40511635a397
ivinogradov <ilya.vngrdv@gmail.com>
2018-10-01 14:00:51 -0500

    Master/detail components
    Summary:
    
    Reviewers: tyrell, patrickcharlesmayer, gilicaspi, zerodiff, alex, thefisch63
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,7 +12,7 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes = HEROES;
 
-  onSelect(hero: Hero):void {
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
